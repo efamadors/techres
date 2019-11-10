f0edd11415a6c07fe16ef3497a87248f12a07493
Fabricio Amador <efamadors@hotmail.com>
2019-11-10 13:10:16 -0600

    db

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express');
+const app = require('express')();
+const https = require('https');
+const fs = require('fs');
 const jsonServer = require('json-server');
+var cors = require('cors');
 
-const app = express();
+app.use(cors());
 
-app.use('/api', jsonServer.router('db.json'));
+app.use('/', jsonServer.router('db.json'));
 
-app.listen(8080);
+// we will pass our 'app' to 'https' server
+https.createServer({
+    key: fs.readFileSync('./privateKey.key'),
+    cert: fs.readFileSync('./certificate.crt'),
+    passphrase: 'TechRes'
+}, app).listen(3000);
